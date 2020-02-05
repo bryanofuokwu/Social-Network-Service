323498618eaeecbb93181fa2f741aaa726a429dd
Roesha Nigos <ishanigos@Roeshas-MacBook-Pro.local>
2020-02-05 16:50:25 -0600

    client follow different

--- social_client.cc
+++ social_client.cc
@@ -215,7 +215,6 @@ public:
             ss << followreq.fr_timestamp().seconds();
             std::string ts = ss.str();
             user_to_follow.append(ts);
-            user_to_follow.append(ts);
             cout << user_to_follow <<endl;
             std::strcpy(buff, user_to_follow.c_str());
             int filewrite = open(fname_f, O_WRONLY);
